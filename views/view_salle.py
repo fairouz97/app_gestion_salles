947034f5bb8954f31e4cbbb75fc8b4ca00edc16e
fairouz <300155881@monboreal.ca>
2026-04-19 21:07:03 -0400

    Ajout cadre Informations Salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -9,3 +9,26 @@ class ViewSalle(ctk.CTk):
         self.geometry("700x500")
 
         self.service_salle = ServiceSalle()
+        # Cadre Informations Salle
+        self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
+        self.cadreInfo.pack(pady=10, padx=10)
+
+        self.labelCode = ctk.CTkLabel(self.cadreInfo, text="Code salle")
+        self.labelCode.grid(row=0, column=0, padx=10, pady=5)
+        self.entryCode = ctk.CTkEntry(self.cadreInfo)
+        self.entryCode.grid(row=0, column=1, padx=10, pady=5)
+
+        self.labelDescription = ctk.CTkLabel(self.cadreInfo, text="Description")
+        self.labelDescription.grid(row=1, column=0, padx=10, pady=5)
+        self.entryDescription = ctk.CTkEntry(self.cadreInfo)
+        self.entryDescription.grid(row=1, column=1, padx=10, pady=5)
+
+        self.labelCategorie = ctk.CTkLabel(self.cadreInfo, text="Catégorie")
+        self.labelCategorie.grid(row=2, column=0, padx=10, pady=5)
+        self.entryCategorie = ctk.CTkEntry(self.cadreInfo)
+        self.entryCategorie.grid(row=2, column=1, padx=10, pady=5)
+
+        self.labelCapacite = ctk.CTkLabel(self.cadreInfo, text="Capacité")
+        self.labelCapacite.grid(row=3, column=0, padx=10, pady=5)
+        self.entryCapacite = ctk.CTkEntry(self.cadreInfo)
+        self.entryCapacite.grid(row=3, column=1, padx=10, pady=5)
