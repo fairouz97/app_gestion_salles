884e0fbc6d74cd2dc6cc420ffc52c5e801accc98
fairouz <300155881@monboreal.ca>
2026-04-19 20:02:21 -0400

    Ajout methode rechercher_salle Service

--- services/service_salle.py
+++ services/service_salle.py
@@ -34,3 +34,5 @@ class ServiceSalle:
     def supprimer_salle(self, code):
         self.dao_salle.delete_salle(code)
         return True, "Salle supprimée"
+    def rechercher_salle(self, code):
+        return self.dao_salle.get_salle(code)
